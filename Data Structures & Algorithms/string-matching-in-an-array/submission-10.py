215262d69dd85b32fb4adc99897453a83fd13e48
Xi (Joy) Wang <106763516+XiWang16@users.noreply.github.com>
2026-04-16 11:39:46 +0100

    Add: string-matching-in-an-array - submission-10

---
+++ Data Structures & Algorithms/string-matching-in-an-array/submission-10.py
@@ -0,0 +1,21 @@
+class Solution:
+    def stringMatching(self, words: List[str]) -> List[str]:
+        """
+        Args:
+            words(List[str]): array of strings (non-empty, lowercase English letters only)
+
+        Returns:
+            List(str): all words that are a substring of another word in words
+        """
+
+        ans = []
+        words.sort(key=len)
+        
+        for i in range(len(words)):
+            cur_word = words[i]
+            for j in range(i + 1, len(words)):
+                candidate_word = words[j]
+                if cur_word in candidate_word:
+                    ans.append(cur_word)
+                    break
+        return ans
